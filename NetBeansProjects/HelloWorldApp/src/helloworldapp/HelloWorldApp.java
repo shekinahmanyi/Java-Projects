7f3c1fc62df855175afca219294d6a7f266a0222
unknown <shekinahmanyi@gmail.com>
2022-05-26 08:13:43 +0100

    Java projects

---
+++ NetBeansProjects/HelloWorldApp/src/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworldapp;
+public class HelloWorldApp {
+    public static void main(String[] args) {
+      System.out.printf("%s\n%s\n",
+       "Welcome to", "Java\nProgramming!");
+    }
+    
+}
---
+++ NetBeansProjects/ReadFiveIntegers/src/readfiveintegers/ReadFiveIntegers.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package readfiveintegers;
+public class ReadFiveIntegers {
+    public static void main(String[] args) {
+        int[] anArray = {100,200,300,400,500};
+        int i;
+        for(i=0;i<=4;i++){
+            System.out.print(anArray[i]);
+            System.out.print("\n");
+        }
+        }
+    }
